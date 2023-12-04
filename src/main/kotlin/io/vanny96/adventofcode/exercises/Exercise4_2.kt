6b147dcda4fc162e9274287ba9f4c7df693186f6
Giovanni Barbaro <vanni-96@hotmail.it>
2023-12-04 07:29:45 +0100

    Adds day 4 part 2 functional approach

--- src/main/kotlin/io/vanny96/adventofcode/exercises/Exercise4_2.kt
+++ src/main/kotlin/io/vanny96/adventofcode/exercises/Exercise4_2.kt
@@ -9,20 +9,48 @@ fun main() {
     val games = exerciseData.lines()
         .map { Game4.fromGameInfo(it) }
 
+    val copiesMap = games
+        .asSequence()
+        .map { it.playerNumbers.intersect(it.winningNumbers).size }
+        .mapIndexed { index, winningResult -> (index + 1)..(index + winningResult) }
+        .map { it.associateWith { 1 } }
+        .reduceIndexed { index, acc, map -> mergeCardMaps(index, acc, map) }
+
+    val result = copiesMap
+        .map { it.value }
+        .sum() + games.size
+
+    println(result)
+}
+
+fun mergeCardMaps(gameId: Int, accumulationMap: Map<Int, Int>, cardResults: Map<Int, Int>): Map<Int, Int> {
+    val cardCopies = accumulationMap[gameId] ?: 0
+    val modifiedMap = cardResults.map { it.key to it.value * (1 + cardCopies) }.toMap()
+    return (accumulationMap.toList() + modifiedMap.toList())
+        .groupBy({ it.first }, { it.second })
+        .map { entryKey -> entryKey.key to entryKey.value.sum() }
+        .toMap()
+}
+
+fun firstApproach() {
+    val exerciseData = textFromResource("inputs/exercise_4.txt") ?: return
+    val games = exerciseData.lines()
+        .map { Game4.fromGameInfo(it) }
+
     val copiesCounter: MutableMap<Int, Int> = mutableMapOf()
-    var highestCopyId = 1
     for (game in games) {
         val numberOfCopies = copiesCounter[game.id] ?: 0
         val winningNumbers = game.playerNumbers.intersect(game.winningNumbers).size
-        val affectedGames = game.id+1..game.id+winningNumbers
+        val affectedGames = (game.id + 1)..(game.id + winningNumbers)
 
         affectedGames.forEach { copiesCounter[it] = (copiesCounter[it] ?: 0) + (1 + numberOfCopies) }
-        highestCopyId = max(highestCopyId, affectedGames.last)
     }
 
     val result = copiesCounter
-        .map{ it.value }
+        .map { it.value }
         .sum() + games.size
+
+
 
     println(result)
 }
